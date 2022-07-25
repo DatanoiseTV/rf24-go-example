a96b9e49e8ebaf5c74453be8a9799ed342cc603d
DatanoiseTV <noreply@datanoise.net>
2022-07-25 18:16:18 +0200

    Add example.

--- main.go
+++ main.go
@@ -3,34 +3,37 @@ package main
 import (
 	"fmt"
         "os"
-	"encoding/binary"
+        "time"
 	radio "github.com/DatanoiseTV/rf24-go"
 )
 
 func main() {
+	fmt.Println("nRF24L01+ from GoLang)
         uid := os.Getuid()
         if uid != 0 {
             fmt.Println("Must be running as root.")
             os.Exit(1)    
         } 
 
-	var pipe uint64 = 0xF0F0F0F0E1
+	var pipe uint64 = 0x65646f4e32
 	r := radio.New(22, 0)
 	defer r.Delete()
 	r.Begin()
-	r.SetRetries(15, 15)
+        r.SetChannel(110)
+	r.SetRetries(3, 3)
 	r.SetAutoAck(true)
-	r.OpenReadingPipe(1, pipe)
-	r.StartListening()
+	r.SetDataRate(radio.RATE_250KBPS)
+        r.SetPALevel(radio.PA_LOW)
+        r.EnableDynamicPayloads()
+        r.OpenWritingPipe(pipe)
+	//r.OpenReadingPipe(1, pipe)
+	r.StopListening()
 	r.PrintDetails()
+
+
 	for {
-		if r.Available() {
-			data, _ := r.Read(4)
-			fmt.Printf("data: %v\n", data)
-			payload := binary.LittleEndian.Uint32(data)
-			fmt.Printf("Received %v\n", payload)
-		} else {
-			//
-		}
+            resp := r.Write([]byte{0x42, 0x44}, 2)
+            fmt.Println(resp) 
+            time.Sleep(1 * time.Second)
 	}
 }
